7e633a721b596d81500d068323e4a33bd278ba88
dentist229 <danielvarennikov@gmail.com>
2020-09-30 15:41:35 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <signal.h>
 #include "window.h"
 #include "shader.h"
 #include "figures.h"
+
+//Stop running if GL throws an error on the log call
+#define ASSERT(TEST) if(!(TEST)) raise(SIGTRAP);
+
+//Wrap functions to Clear errors and crash
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
 using namespace std;
+
+static void GLClearError(){
+
+    //Clean all the errors
+    while(glGetError() != GL_NO_ERROR);
+
+}
+
+static bool GLLogCall(const char* function, const char* file, int line){
+
+    //Print all the errors
+    while (GLenum error = glGetError()){
+        cout << "[GL Error] : " << error << " " << function << " " << file << " : " << line << endl;
+        return false;
+    }
+
+    return true;
+}
 
 int main( int argc, char** argv ){
 
@@ -15,7 +43,7 @@ int main( int argc, char** argv ){
 
     unsigned int shader = createShader( source.VertexSource, source.FragmentSource );
     glUseProgram( shader );
-    
+
     float positions[ 12 ] = {
             -0.5f, -0.5f,
             0.5f, -0.5f,
